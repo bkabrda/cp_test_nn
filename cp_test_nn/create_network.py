88c3f591043f10f20fc5725406ecda0154738c51
Slavek Kabrda <bkabrda@redhat.com>
2017-07-25 14:15:02 +0200

    Training should be method of the Neural Network

--- cp_test_nn/create_network.py
+++ cp_test_nn/create_network.py
@@ -1,11 +1,13 @@
 from cp_test_nn.process import process
 from cp_test_nn.tokenizer import create_token_dict
 from cp_test_nn.dataset import create_dataset
-from cp_test_nn.train import train
+from cp_test_nn.nn import NNWithScaler
 from cp_test_nn.util import FAILS_FILE, TOKEN_DICT_FILE, DATASET_FILE
 
 def create_network(input):
     process(input, FAILS_FILE)
     create_token_dict(FAILS_FILE, TOKEN_DICT_FILE)
     create_dataset(FAILS_FILE, TOKEN_DICT_FILE, DATASET_FILE)
-    return train(DATASET_FILE)
+    nn = NNWithScaler()
+    nn.train(DATASET_FILE)
+    return nn
--- cp_test_nn/nn.py
+++ cp_test_nn/nn.py
@@ -1,8 +1,24 @@
+import json
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+
 class NNWithScaler:
-    def __init__(self, network, scaler):
-        self.network = network
-        self.scaler = scaler
+    def __init__(self):
+        self.network = MLPClassifier(hidden_layer_sizes=(200, 200))
+        self.scaler = StandardScaler()
 
     def predict(self, X):
         X = self.scaler.transform(X)
         return self.network.predict(X)
+
+    def train(self, dataset_file):
+        X, y = [], []
+        with open(dataset_file) as f:
+            loaded = json.load(f)
+        X = loaded['dataset']
+        y = loaded['results']
+
+        self.scaler.fit(X)
+        X = self.scaler.transform(X)
+        self.network.fit(X, y)
